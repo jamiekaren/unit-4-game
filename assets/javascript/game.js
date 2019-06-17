09eb80ba8c9221d071e32087c42a04a4607f3df1
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-06-17 13:35:35 -0400

    Changed background, got sound bite to work, changed Solo image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,55 +4,50 @@ console.log("Working...");
 // let pageAudio = document.createElement("audio");
 // pageAudio.setAttribute("src", "assets\javascript\star-wars-theme.mp3");
 
-let music = new Audio("assets/javascript/star-wars-theme.mp3");
+let themeMusic = new Audio("assets/sounds/star-wars-theme.mp3");
 
-let soloBite = new Audio ("assets/solo-bite.mp3");
+let soloBite = new Audio("assets/sounds/solo-bite.mp3");
 
-$(".theme-button").on("click", function () {
-    music.play();
-});
-$(".pause-button").on("click", function () {
-    music.pause();
-});
 
-$("#solo").on("click", function () {
-    soloBite.play();
-});
-
-let gameMusic = {
-    mainMusic: "",
-    soloSound: $("#solo").on("click", function () {
-        soloBite.play();
-    }),
-    vaderSound: "",
-    skyWalkerSound: "",
-    stormTrooperSound: "",
+let gameMusic = function () {
+    $(".theme-button").on("click", function () {
+        music.play();
+    });
+    $(".pause-button").on("click", function () {
+        music.pause();
+    });
+ 
 };
-
-
 
 //Create some objects to store characters and their stats
 
 
 let vader = {
+    name: 'Darth Vader',
     health: 5000,
     attack: 300,
     counter: 100,
 };
 
 let solo = {
+    name: 'Han Solo',
+    soloSound: $("#solo").on("click", function () {
+        soloBite.play()
+        }),
     health: 2000,
-    attack: 400,
-    counter: 50,
+    attack: 100,
+    counter: 400,
 };
 
 let skyWalker = {
+    name: 'Luke Skywalker',
     health: 3000,
     attack: 250,
     counter: 300,
 };
 
 let stormTrooper = {
+    name: 'Stormtrooper',
     health: 500,
     attack: 100,
     counter: 50,
@@ -60,7 +55,7 @@ let stormTrooper = {
 
 
 //Check all my characters are in fighting condition
-console.log(vader, solo, skyWalker, stormTrooper);
+console.log(vader.name + vader.health, solo.name + solo.health, skyWalker.name + skyWalker.health, stormTrooper.name + stormTrooper.health);
 
 
 //have border of character change color on hover or select
@@ -69,27 +64,24 @@ console.log(vader, solo, skyWalker, stormTrooper);
 //     console.log(solo);
 // };
 
-let gameStart = {
-    selectChar: function (user, enemy) {
-        $("image").on('click').css("border", "blue").addClass("user-chosen");
-        $("image").on('click').css("border", "red").addClass("enemy-chosen");
-    },
-    moveChar: function (user, enemy) {
-        if (selectChar == true)
-            $(selectChar).append("#user-character");
+// let gameStart = {
+//     selectChar: function (user, enemy) {
+//         $("image").on('click').css("border", "blue").addClass("user-chosen");
+//         $("image").on('click').css("border", "red").addClass("enemy-chosen");
+//     },
+//     moveChar: function (user, enemy) {
+//         if (selectChar == true)
+//             $(selectChar).append("#user-character");
 
-        else if (!selectChar)
-            $().append("#enemy-character")
-    },
+//         else if (!selectChar)
+//             $().append("#enemy-character")
+//     },
 
-};
+// };
 
 
 
 //When character is selected, move remaining images to left column
 //Also move selected character to left of screen (characters start in the middle)
-//Create on click event for "Fight" button
-
-
-
+//Create on click event for "Fight" button 
 
