c4f6f7b64bed7cd93940fb08425a045cdd739eee
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-06-17 00:03:51 -0400

    I should be sleeping. Tried added some completely unnecessary sound and music to characters and page, not sucessful yet.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,36 @@
 console.log("Working...");
 
+//Fight game music
+// let pageAudio = document.createElement("audio");
+// pageAudio.setAttribute("src", "assets\javascript\star-wars-theme.mp3");
+
+let music = new Audio("assets\javascript\star-wars-theme.mp3");
+
+let soloBite = new Audio ("assets\solo-bite.mp3");
+
+$(".theme-button").on("click", function () {
+    music.play();
+});
+$(".pause-button").on("click", function () {
+    music.pause();
+});
+
+$("#solo").on("click", function () {
+    soloBite.play();
+});
+
+let gameMusic = {
+    mainMusic = "",
+    soloSound = $("#solo").on("click", function () {
+        soloBite.play();
+    }),
+    vaderSound = "",
+    skyWalkerSound = "",
+    stormTrooperSound = "",
+};
+
+
+
 //Create some objects to store characters and their stats
 
 
@@ -34,7 +65,22 @@ let stormTrooper = {
 //     console.log(solo);
 // };
 
-$("#solo").on('click').css("border", "blue");
+let gameStart = {
+    selectChar = function (user, enemy) {
+        $("image").on('click').css("border", "blue").addClass("user-chosen");
+        $("image").on('click').css("border", "red").addClass("enemy-chosen");
+    },
+    moveChar = function (user, enemy) {
+        if (selectChar == true)
+            $(selectChar).append("#user-character");
+
+        else if (!selectChar)
+            $().append("#enemy-character")
+    },
+
+};
+
+
 
 //When character is selected, move remaining images to left column
 //Also move selected character to left of screen (characters start in the middle)
