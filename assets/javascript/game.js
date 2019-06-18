d7853b48430501c0b62ebbc8c7419d110240862f
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-06-18 18:40:03 -0400

    Have user selections working to console log, but now have to figure out how to move the character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,141 +28,144 @@ $("#pause-button").on("click", function () {
 //     console.log('Playing theme music');
 // });
 
-//store whether a character is selected
-let userChar = false;
-let enemyChar = false;
-let userDead = false;
-let enemyDead = false;
-
-
-//start game with some logical if statements...hopefully...
-
-function gameStart() {
-    $(".char").on('click', function () {
-        if (userChar == false) {
-            userChar = true;
-            console.log(userChar + 'ready');
-            $(".this").prependTo("#user-character");
-        } else if (userChar == true && enemyChar == false) {
-            enemyChar = true;
-            $(".this").prependTo("#enemy-character");
-        } else if (userChar == true && enemyChar == true) {
-            alert('Attack the enemy!');
-        }
-    });
-
-    //Make an attack button function
-
-
-    /*minus user attack number from enemy
-    then minus enemy counter-attack number from user
-    then display user attack stats in sentence on document
-    then add user attack number back into itself
-    then display enemy attack stats in sentence on document
-    if health <= to 0 , set dead variable to true, run heaven function
-
-        */
-
-    function attack() {
-        $('#attack-button').click(function () {
-            fightBite.play();
-            console.log("It's on like donkey kong");
-
-            if (!userDead) {
-                
-            }
-
-        });
-
-    };
 
 
 
-    /*
-    when a character dies, they go to character heaven
-    Need to select object health property in user character div
-    and enemy char div
-    if health <= 0 then set button to hidden
+/*minus user attack number from enemy
+then minus enemy counter-attack number from user
+then display user attack stats in sentence on document
+then add user attack number back into itself
+then display enemy attack stats in sentence on document
+if health <= to 0 , set dead variable to true, run heaven function
+
     */
 
-    function heaven() {
-        if (userDead == true)
-            $("#user-character").hidden();
-        else if (enemyDead = true)
-            $("#enemy-character").remove("btn");
-    };
+
+//move character once selected 
+
+//    function moveChar (char, area) {
+//     $(char).on("click", function () {
+//         $(char).prependTo(area);
+//     });
+// };
+
+// select your character
+
+let userChar = 'user';
+let enemyChar = 'enemy';
+
+console.log(userChar);
+console.log(enemyChar);
+
+$("#characters-area").find("button").click(function () {
+
+    if (userChar === 'user') {
+        fightBite.play();
+        userChar = ($(this).attr('id'));
+        console.log('Your Character is ' + userChar + (typeof(userChar)));
+        $(userChar).replaceWith('#' + userChar).prependTO("#user-character");
+        
+        // document.getElementById(userChar).append("#user-character");
+
+    } else if (enemyChar === 'enemy') {
+        enemyChar = ($(this).attr('id'));
+        console.log('Your Enemy is ' + enemyChar);
+    }
 
 
-    //Create some objects to store characters and their stats
+});
 
-    let solo = {
-        name: 'Han Solo',
-        soloSound: $("#solo").on("click", function () {
-            soloBite.play();
-            console.log('Sound bite works!');
-        }),
-        health: 2000,
-        attack: 50,
-        counter: 200,
-    };
-
-    let droid = {
-
-        name: 'R2-D2',
-        r2Sound: $("#droid").on("click", function () {
-            r2Bite.play();
-            console.log('Sound bite works!');
-        }),
-        health: 2000,
-        attack: 50,
-        counter: 200,
-    };
+// if (userChar === 'solo') {
+//     moveChar ("#solo", "#user-character"); 
+// } else ()
 
 
-    let skyWalker = {
 
-        name: 'Luke Skywalker',
-        lukeSound: $("#luke").on("click", function () {
-            lukeBite.play();
-            console.log('Sound bite works!');
-        }),
-        health: 3000,
-        attack: 100,
-        counter: 50,
-    };
+/*
+when a character dies, they go to character heaven
+Need to select object health property in user character div
+and enemy char div
+if health <= 0 then set button to hidden
+*/
+
+function heaven() {
+    if (userDead == true)
+        $("#user-character").hidden();
+    else if (enemyDead = true)
+        $("#enemy-character").remove("btn");
+};
 
 
-    let stormTrooper = {
+//Create some objects to store characters and their stats
 
-        name: 'Stormtrooper',
-        trooperSound: $("#trooper").on("click", function () {
-            trooperBite.play();
-        }),
-        health: 500,
-        attack: 20,
-        counter: 10,
-    };
+let solo = {
+    name: 'Han Solo',
+    soloSound: $("#solo").on("click", function () {
+        soloBite.play();
+        console.log('Sound bite works!');
+    }),
+    health: 2000,
+    attack: 50,
+    counter: 200,
+};
 
-    let greedo = {
+let droid = {
 
-        name: 'Greedo',
-        greedoSound: $("#greedo").on("click", function () {
-            greedoBite.play();
-        }),
-        health: 5000,
-        attack: 100,
-        counter: 50,
-    };
+    name: 'R2-D2',
+    r2Sound: $("#droid").on("click", function () {
+        r2Bite.play();
+        console.log('Sound bite works!');
+    }),
+    health: 2000,
+    attack: 50,
+    counter: 200,
+};
 
-    //Check all my characters are in fighting condition
-    console.log(greedo.name + greedo.health, solo.name + solo.health, skyWalker.name + skyWalker.health, stormTrooper.name + stormTrooper.health, droid.name + " health:" + droid.health);
 
-    //Add name and health stats to character html
-    $("#solo").prepend(solo.name + "<br>" + "Health: " + solo.health);
-    $("#luke").prepend(skyWalker.name + "<br>" + "Health: " + skyWalker.health);
-    $("#droid").prepend(droid.name + "<br>" + "Health: " + droid.health);
-    $("#trooper").prepend(stormTrooper.name + "<br>" + "Health: " + stormTrooper.health);
-    $("#greedo").prepend(greedo.name + "<br>" + "Health: " + greedo.health);
+let skyWalker = {
+
+    name: 'Luke Skywalker',
+    lukeSound: $("#luke").on("click", function () {
+        lukeBite.play();
+        console.log('Sound bite works!');
+    }),
+    health: 3000,
+    attack: 100,
+    counter: 50,
+};
+
+
+let stormTrooper = {
+
+    name: 'Stormtrooper',
+    trooperSound: $("#trooper").on("click", function () {
+        trooperBite.play();
+    }),
+    health: 500,
+    attack: 20,
+    counter: 10,
+};
+
+let greedo = {
+
+    name: 'Greedo',
+    greedoSound: $("#greedo").on("click", function () {
+        greedoBite.play();
+    }),
+    health: 5000,
+    attack: 100,
+    counter: 50,
+};
+
+//Check all my characters are in fighting condition
+console.log(greedo.name + greedo.health, solo.name + solo.health, skyWalker.name + skyWalker.health, stormTrooper.name + stormTrooper.health, droid.name + " health:" + droid.health);
+
+//Add name and health stats to character html
+$("#solo").prepend(solo.name + "<br>" + "Health: " + solo.health);
+$("#luke").prepend(skyWalker.name + "<br>" + "Health: " + skyWalker.health);
+$("#droid").prepend(droid.name + "<br>" + "Health: " + droid.health);
+$("#trooper").prepend(stormTrooper.name + "<br>" + "Health: " + stormTrooper.health);
+$("#greedo").prepend(greedo.name + "<br>" + "Health: " + greedo.health);
 
 //move character function
 
@@ -170,11 +173,7 @@ function gameStart() {
 
 // console.log(greedo.health);
 
-// function moveChar (char, area) {
-//     $(char).on("click", function () {
-//         $(char).prependTo(area);
-//     });
-// };
+
 
 
 
